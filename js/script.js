11919cb70e6ec8ec8bb715ae89ac2a3212bdeec4
hahaminja <haha_mj@naver.com>
2022-07-31 23:44:57 +0900

    업로드

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ $(document).ready(function(){
     autoplaySpeed : 3000 ,
     dots: true,
     arrows : false,
+    pauseOnHover: false,
   });
 
   $('.list_slide_wrap').slick({
@@ -12,5 +13,35 @@ $(document).ready(function(){
     arrows:false,
     autoplay : true ,
     autoplaySpeed : 2000 ,
+    pauseOnHover: false,
+
   });
+
+  $('.slides-wrap').slick({
+    dots:true,
+    autoplay:true,
+    autoplaySpeed : 3000,
+    arrows:false,
+    pauseOnHover: false,
+
+  });
+
 });
+function Topbar__init(){
+  var $Line = $(".top_menu_box_sub_line");
+
+  $(".top_menu_box ul > li").mouseenter(function(){
+
+    var $li = $(this);
+
+    var left = $li.position().left;
+    var width = $li.outerWidth();
+
+    $Line.css({
+      left: left,
+      width: width
+    });
+  });
+  $(".top_menu_box ul > li").eq(0).mouseenter();
+}
+Topbar__init();
