c9fab49730e4920d6f3f19f4472001ed8bfa2c07
hahaminja <haha_mj@naver.com>
2022-08-03 12:00:58 +0900

    업로드

--- js/script.js
+++ js/script.js
@@ -1,16 +1,80 @@
-gsap.registerPlugin(ScrollTrigger);
-
-// 슬라이드
+// 슬릭 슬라이드
 $(document).ready(function(){
-  $('.slider-wrap').slick({
+  // 메인배너슬라이드 첫번째페이지 글씨 올라오기
+$('.slider-wrap').on('init', function(event, slick){
+  $(".slider-wrap .banner")
+  .eq(1)
+  .find($(".text-box"))
+  .addClass("active");
+});
+// 캠페인배너 첫번째페이지 글씨 올라오기
+$('.slides-wrap').on('init', function(event, slick){
+  $(".slides-wrap .content")
+  .eq(1)
+  .find($(".text-box"))
+  .addClass("active");
+});
+
+// 스크롤탑바 버튼
+$(".up_btn").click(function(){
+  $("html,body").animate({scrollTop: 0}, 600);
+});
+
+$(window).scroll(function(){
+
+  var $scrollTop = $(this).scrollTop();
+
+  if ($scrollTop > 1000) {
+    $(".up_btn").css({ opacity: 1, visibility: "visible" });
+  } else {
+    $(".up_btn").css({ opacity: 0, visibility: "hidden" });
+  }
+
+});
+// 스크롤탑바 버튼 끝
+// 서브메뉴 엑티브 시작
+// $(".top_menu_box li").mouseenter(function(){
+//   // var thisIndex = $(this).index()
+
+//   $(".top_menu_box_sub_menu .content").addClass("active");
+//   $(".top_menu_box_sub_menu").addClass("active");
+// })
+
+// $(".top_menu_box li").mouseleave(function(){
+// //   // var thisIndex = $(this).index()
+
+//   $(".top_menu_box_sub_menu .content").removeClass("active");
+//   $(".top_menu_box_sub_menu").addClass("active");
+
+// $(".top_menu_box_sub_menu .content").mouseenter(function(){
+//   $(".top_menu_box_sub_menu").addClass("active");
+//   $(".top_menu_box_sub_menu .content").addClass("active");
+// })
+
+// });
+
+
+// 서브메뉴 엑티브 끝
+  $(".slider-wrap").slick({
     autoplay : true ,
     autoplaySpeed : 3000 ,
     dots: true,
     arrows : false,
     pauseOnHover: false,
   });
+  // 메인배너슬라이드 글씨 올라오기
+  $(".slider-wrap").on("afterChange", function (event, slick, currentSlide) {
+    $(".slider-wrap .banner").find($(".text-box")).removeClass("active");
 
-  $('.list_slide_wrap').slick({
+    $(".slider-wrap .banner")
+      .eq(currentSlide + 1)
+      .find($(".text-box"))
+      .addClass("active");
+  });
+
+
+// 리스트_슬라이드
+  $(".list_slide_wrap").slick({
     slidesToShow: 4,
     swipeToSlide: true,
     arrows:false,
@@ -19,22 +83,30 @@ $(document).ready(function(){
     pauseOnHover: false,
 
   });
-
-  $('.slides-wrap').slick({
+// 캠페인 배너 슬라이드
+  $(".slides-wrap").slick({
     dots:true,
     autoplay:true,
     autoplaySpeed : 3000,
     arrows:false,
     pauseOnHover: false,
   });
-  
+  // 캠페인 배너에 글씨 올라오기
+  $(".slides-wrap").on("afterChange", function (event, slick, currentSlide) {
+    $(".slides-wrap .content").find($(".text-box")).removeClass("active");
+
+    $(".slides-wrap .content")
+      .eq(currentSlide + 1)
+      .find($(".text-box"))
+      .addClass("active");
+  });
 
 });
 // 헤더 탑바_밑줄
 function Topbar__init(){
   var $Line = $(".top_menu_box_sub_line");
 
-  $(".top_menu_box ul > li").mouseenter(function(){
+  $(".top_menu_box > ul > li > a").mouseenter(function(){
 
     var $li = $(this);
 
@@ -43,18 +115,29 @@ function Topbar__init(){
 
     $Line.css({
       left: left,
-      width: width
+      width: width,
     });
   });
-  $(".top_menu_box ul > li").eq(0).mouseenter();
+  $(".top_menu_box > ul > li > a").eq(0).mouseenter();
 }
 Topbar__init();
 
 
-gsap.from(".footer_img > img",{
-  x:-500,
-  duration:3,
-  opacity:0,
+// 스크롤트리거ㅠㅠㅠㅠ
+gsap.registerPlugin(ScrollTrigger);
 
-
+gsap.from(".footer_img > img", {
+  duration: 1,
+  x:-800,
+  opacity: 0,
+  scrollTrigger: {
+    trigger: ".footer_img > img",
+    markers:true,
+    start:"top 80%",
+    end:"bottom 30%",
+    
+  }
 });
+
+
+
